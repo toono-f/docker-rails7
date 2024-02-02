94f249e0b5d8f12d458e7583069f429d9ad3bc15
toono-f <52028799+toono-f@users.noreply.github.com>
2024-02-02 21:43:33 +0900

    Add Todo model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_925_083_921) do
+ActiveRecord::Schema[7.0].define(version: 20_240_202_123_754) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
+
+  create_table 'todos', force: :cascade do |t|
+    t.string 'title'
+    t.boolean 'completed'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+  end
 
   create_table 'users', force: :cascade do |t|
     t.string 'name'
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@ users = [
 users.each do |name, age, hobby|
   User.create(name:, age:, hobby:)
 end
+
+todos = [
+  ['サッカーを観戦', false],
+  ['野球を観戦', false],
+  ['バレーを観戦', false]
+]
+
+todos.each do |title, completed|
+  Todo.create(title:, completed:)
+end

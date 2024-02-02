e074f8fe62828415a35ad5090d9db3a9dc33b29d
toono-f <52028799+toono-f@users.noreply.github.com>
2024-02-02 21:37:34 +0900

    Update schema.rb with correct version and use single quotes for enable_extension

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_25_083921) do
+ActiveRecord::Schema[7.0].define(version: 20_220_925_083_921) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.integer "age"
-    t.string "hobby"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.integer 'age'
+    t.string 'hobby'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
